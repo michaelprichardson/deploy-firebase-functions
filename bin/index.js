f7165c36a864c8cb3a6df313b56576a8bb3cfbec
Michael Richardson <michael@richardsons.co.za>
2020-10-29 20:43:09 +0000

    Tidy up code

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,7 @@
 const chalk = require('chalk');
 var argv = require('minimist')(process.argv.slice(2));
 var shell = require('shelljs');
+var path = require('path');
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -9,27 +10,35 @@ function sleep(ms) {
 
 async function runDeployCommands(args) {
     if (args.h || args.help) {
-        console.log(chalk.magenta('deploy-firebase-functions: ') + 'available arguments');
+        console.log(chalk.magenta('deploy-firebase-functions: '));
+        console.log();
+        console.log('available arguments:');
         console.log('   --index (string) This is the file exporting all the files for Firebase. Defaults to functions/index.js');
-        console.log('   --max (int) This is the maximum number of functions to deploy at once. Defaults to 10');
+        console.log('   --max (int) This is the maximum number of functions to deploy at once. Defaults to 10 (Max 10)');
+        console.log('   --pause (int) This is the time (ms) to wait between each deploy. Defaults to 2500ms');
+        console.log();
         console.log(chalk.red('Note: Typescript is not supported'));
         return;
     }
 
+    // Assign the variables needed
     var filePath = 'functions/index.js';
     var maxFuncs = 5;
     var pause = 2500;
     if (args.index) {
         filePath = argv.index;
     }
-
     if (args.max) {
-        maxFuncs = argv.max;
+        maxFuncs = min(argv.max, 10);
+    }
+    if (args.pause) {
+        pause = argv.pause;
     }
 
     console.log('Processing functions in: ' + chalk.magenta(filePath));
 
-    const imports = require(filePath);
+    // Collect the functions from the index file
+    const imports = require(path.join(process.cwd(), filePath));
     const funcNames = Object.keys(imports);
 
     console.log(`There ${funcNames.length === 1 ? 'is' : 'are'} ${chalk.magenta(funcNames.length)} function${funcNames.length === 1 ? '' : 's'} to deploy to Firebase!`);
@@ -37,6 +46,7 @@ async function runDeployCommands(args) {
     const deployCmd = 'firebase deploy --only ';
     const funcCmd = 'functions:';
 
+    // Loop through all the functions and group them and build the command to execute
     var ii, jj, cmd = '';
     for (ii = 0, jj = funcNames.length; ii < jj; ii += maxFuncs) {
         const groupedNames = funcNames.slice(ii, ii + maxFuncs);
@@ -54,6 +64,7 @@ async function runDeployCommands(args) {
         
         cmd = '';
 
+        // Only pause if it is not the last command executed
         if (ii < funcNames.length - 1) {
             console.log(`Pausing for ${pause}ms`);
             await sleep(2500);

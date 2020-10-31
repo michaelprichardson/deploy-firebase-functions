568edca912bc4df7be47c4d06f2989cb5cfc78f8
Michael Richardson <michael@richardsons.co.za>
2020-10-31 09:47:32 +0000

    fix: Add retry so that delete can be used

--- bin/index.js
+++ bin/index.js
@@ -13,6 +13,7 @@ async function runDeployCommands(args) {
         console.log(chalk.magenta('deploy-firebase-functions: '));
         console.log();
         console.log('available arguments:');
+        console.log('   --retry (bool) This will try a re-deploy the failed functions until it succeeds (Max retried is 10)');
         console.log('   --index (string) This is the file exporting all the files for Firebase. Defaults to functions/index.js');
         console.log('   --max (int) This is the maximum number of functions to deploy at once. Defaults to 10 (Max 10)');
         console.log('   --pause (int) This is the time (ms) to wait between each deploy. Defaults to 2500ms');
@@ -22,9 +23,14 @@ async function runDeployCommands(args) {
     }
 
     // Assign the variables needed
+    var retry = true;
+    var maxRetries = 10;
     var filePath = 'functions/index.js';
     var maxFuncs = 5;
     var pause = 2500;
+    if (args.retry) {
+        retry = argv.retry;
+    }
     if (args.index) {
         filePath = argv.index;
     }
@@ -35,41 +41,63 @@ async function runDeployCommands(args) {
         pause = argv.pause;
     }
 
-    console.log('Processing functions in: ' + chalk.magenta(filePath));
+    
 
-    // Collect the functions from the index file
-    const imports = require(path.join(process.cwd(), filePath));
-    const funcNames = Object.keys(imports);
+    if (retry) {
+        console.log(`Deploying all functions at once and will retry until successful (max retries: ${maxRetries})`);
 
-    console.log(`There ${funcNames.length === 1 ? 'is' : 'are'} ${chalk.magenta(funcNames.length)} function${funcNames.length === 1 ? '' : 's'} to deploy to Firebase!`);
+        const errorRegex = /(?<=\[)(.*?)(?=\(.*?\)\]: Deployment error.)/g;
+        const commandRegex = /firebase deploy --only \".*\"/;
+        var response = shell.exec('firebase deploy --only functions');
+        var firebaseOutput = response.stdout;
 
-    const deployCmd = 'firebase deploy --only ';
-    const funcCmd = 'functions:';
-
-    // Loop through all the functions and group them and build the command to execute
-    var ii, jj, cmd = '';
-    for (ii = 0, jj = funcNames.length; ii < jj; ii += maxFuncs) {
-        const groupedNames = funcNames.slice(ii, ii + maxFuncs);
-
-        for (var idx = 0; idx < groupedNames.length; idx++) {
-            cmd += `${funcCmd}${groupedNames[idx]}${idx < groupedNames.length-1 ? ',' : ''}`;
+        while (response.stdout.match(errorRegex).length > 0) {
+            // Get the command to execute provided by Firebase
+            const command = firebaseOutput.match(commandRegex);
+            if (command.length > 0) {
+                response = shell.exec(command[0]);
+            } else {
+                console.log('Couldn\'t find any error with the deploy, exiting.');
+                return;
+            }
         }
-        const executeCommand = `${deployCmd}${cmd}`;
-        console.log(`${chalk.magenta('Execute command:')} ${executeCommand}`);
-        console.log(`${chalk.magenta('Firebase output: ')}`);
-        const response = shell.exec(executeCommand);
-        if (response.code !== 0) {
-            return;
-        }
+    } else {
+        console.log('Processing functions in: ' + chalk.magenta(filePath));
+
+        // Collect the functions from the index file
+        const imports = require(path.join(process.cwd(), filePath));
+        const funcNames = Object.keys(imports);
+
+        console.log(`There ${funcNames.length === 1 ? 'is' : 'are'} ${chalk.magenta(funcNames.length)} function${funcNames.length === 1 ? '' : 's'} to deploy to Firebase!`);
         
-        cmd = '';
+        const deployCmd = 'firebase deploy --only ';
+        const funcCmd = 'functions:';
 
-        // Only pause if it is not the last command executed
-        if (ii < funcNames.length - 1) {
-            console.log(`Pausing for ${pause}ms`);
-            await sleep(2500);
+        // Loop through all the functions and group them and build the command to execute
+        var ii, jj, cmd = '';
+        for (ii = 0, jj = funcNames.length; ii < jj; ii += maxFuncs) {
+            const groupedNames = funcNames.slice(ii, ii + maxFuncs);
+
+            for (var idx = 0; idx < groupedNames.length; idx++) {
+                cmd += `${funcCmd}${groupedNames[idx]}${idx < groupedNames.length-1 ? ',' : ''}`;
+            }
+            const executeCommand = `${deployCmd}${cmd}`;
+            console.log(`${chalk.magenta('Execute command:')} ${executeCommand}`);
+            console.log(`${chalk.magenta('Firebase output: ')}`);
+            const response = shell.exec(executeCommand);
+            if (response.code !== 0) {
+                return;
+            }
+
+            cmd = '';
+
+            // Only pause if it is not the last command executed
+            if (ii < funcNames.length - 1) {
+                console.log(`Pausing for ${pause}ms`);
+                await sleep(2500);
+            }
+
         }
-        
     }
 }
 
